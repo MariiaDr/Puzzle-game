e0a9b210b8e029063e89982f335be27ed549b548
Asimut <mariia.drana@gmail.com>
2023-06-01 09:12:22 +0300

    Upload

---
+++ story_content/user.js
@@ -0,0 +1,247 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5hUzcTocnjK":
+        Script1();
+        break;
+      case "6JRRqzYdz32":
+        Script2();
+        break;
+      case "5wJW725s2z0":
+        Script3();
+        break;
+      case "6mz7z2HttqC":
+        Script4();
+        break;
+      case "6SVc3EPwJIw":
+        Script5();
+        break;
+      case "5o16ku2eKRS":
+        Script6();
+        break;
+      case "6KicUJJnHvF":
+        Script7();
+        break;
+      case "5WutMw5q0rk":
+        Script8();
+        break;
+      case "5fF6FY3zhPo":
+        Script9();
+        break;
+      case "6dRN8v8MVDY":
+        Script10();
+        break;
+      case "5jjPr8UBPUK":
+        Script11();
+        break;
+      case "5vyTspm8cMh":
+        Script12();
+        break;
+      case "5sg88ROYB0t":
+        Script13();
+        break;
+      case "6WMvDvBISke":
+        Script14();
+        break;
+      case "6peRn66sxbJ":
+        Script15();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player=GetPlayer();
+
+var position = player.GetVar("P5");
+var empty = player.GetVar("Empty");
+
+var control = position-empty;
+
+player.SetVar("Control",control);
+
+}
+
+function Script2()
+{
+  var player=GetPlayer();
+
+var position = player.GetVar("P6");
+var empty = player.GetVar("Empty");
+
+var control = position-empty;
+
+player.SetVar("Control",control);
+
+}
+
+function Script3()
+{
+  var player=GetPlayer();
+
+var position = player.GetVar("P7");
+var empty = player.GetVar("Empty");
+
+var control = position-empty;
+
+player.SetVar("Control",control);
+
+}
+
+function Script4()
+{
+  var player=GetPlayer();
+
+var position = player.GetVar("P8");
+var empty = player.GetVar("Empty");
+
+var control = position-empty;
+
+player.SetVar("Control",control);
+
+}
+
+function Script5()
+{
+  var player=GetPlayer();
+
+var position = player.GetVar("P9");
+var empty = player.GetVar("Empty");
+
+var control = position-empty;
+
+player.SetVar("Control",control);
+
+}
+
+function Script6()
+{
+  var player=GetPlayer();
+
+var position = player.GetVar("P10");
+var empty = player.GetVar("Empty");
+
+var control = position-empty;
+
+player.SetVar("Control",control);
+
+}
+
+function Script7()
+{
+  var player=GetPlayer();
+
+var position = player.GetVar("P11");
+var empty = player.GetVar("Empty");
+
+var control = position-empty;
+
+player.SetVar("Control",control);
+
+}
+
+function Script8()
+{
+  var player=GetPlayer();
+
+var position = player.GetVar("P12");
+var empty = player.GetVar("Empty");
+
+var control = position-empty;
+
+player.SetVar("Control",control);
+
+}
+
+function Script9()
+{
+  var player=GetPlayer();
+
+var position = player.GetVar("P13");
+var empty = player.GetVar("Empty");
+
+var control = position-empty;
+
+player.SetVar("Control",control);
+
+}
+
+function Script10()
+{
+  var player=GetPlayer();
+
+var position = player.GetVar("P14");
+var empty = player.GetVar("Empty");
+
+var control = position-empty;
+
+player.SetVar("Control",control);
+
+}
+
+function Script11()
+{
+  var player=GetPlayer();
+
+var position = player.GetVar("P15");
+var empty = player.GetVar("Empty");
+
+var control = position-empty;
+
+player.SetVar("Control",control);
+
+}
+
+function Script12()
+{
+  var player=GetPlayer();
+
+var position = player.GetVar("P1");
+var empty = player.GetVar("Empty");
+
+var control = position-empty;
+
+player.SetVar("Control",control);
+
+}
+
+function Script13()
+{
+  var player=GetPlayer();
+
+var position = player.GetVar("P2");
+var empty = player.GetVar("Empty");
+
+var control = position-empty;
+
+player.SetVar("Control",control);
+
+}
+
+function Script14()
+{
+  var player=GetPlayer();
+
+var position = player.GetVar("P3");
+var empty = player.GetVar("Empty");
+
+var control = position-empty;
+
+player.SetVar("Control",control);
+
+}
+
+function Script15()
+{
+  var player=GetPlayer();
+
+var position = player.GetVar("P4");
+var empty = player.GetVar("Empty");
+
+var control = position-empty;
+
+player.SetVar("Control",control);
+
+}
+
